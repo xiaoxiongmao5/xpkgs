44ef6e96adb1a569519f0a41d591d682fa34aa87
小熊 <627516430@qq.com>
2024-04-06 21:39:21 +0800

    feat: update

--- xfilter/xfilter_test.go
+++ xfilter/xfilter_test.go
@@ -23,7 +23,7 @@ func TestFilterList(t *testing.T) {
 	users = append(users, &User{Name: "a张三", CreatedTm: "2021-01-01"})
 	users = append(users, &User{Name: "李四", CreatedTm: "2021-01-02"})
 	users = append(users, &User{Name: "b王五", CreatedTm: "2021-01-03"})
-	users = append(users, &User{Name: "1赵六", CreatedTm: "2021-01-04"})
+	users = append(users, &User{Name: "21赵六", CreatedTm: "2021-01-04"})
 	users = append(users, &User{Name: "c孙七", CreatedTm: "2021-01-05"})
 	users = append(users, &User{Name: "周八", CreatedTm: "2021-01-06"})
 	users = append(users, &User{Name: "吴九", CreatedTm: "2021-01-07"})
@@ -33,14 +33,14 @@ func TestFilterList(t *testing.T) {
 	list := FilterList[*User](users,
 		&SortFilter[*User]{SortType: "asc", SortField: "name"},
 	)
-	expectList := []string{"1赵六", "a张三", "b王五", "c孙七", "李四", "吴九", "郑十", "周八"}
+	expectList := []string{"21赵六", "a张三", "b王五", "c孙七", "李四", "吴九", "郑十", "周八"}
 	Check(t, list, expectList)
 
 	// 按照创建时间降序排序
 	list = FilterList[*User](users,
 		&SortFilter[*User]{SortType: "desc", SortField: "createdTm"},
 	)
-	expectList = []string{"郑十", "吴九", "周八", "c孙七", "1赵六", "b王五", "李四", "a张三"}
+	expectList = []string{"郑十", "吴九", "周八", "c孙七", "21赵六", "b王五", "李四", "a张三"}
 	Check(t, list, expectList)
 
 	// 按照创建时间升序排序，分页
@@ -56,7 +56,7 @@ func TestFilterList(t *testing.T) {
 		&SortFilter[*User]{SortType: "desc", SortField: "name"},
 		&PageFilter[*User]{PageNo: 3, PageSize: 3},
 	)
-	expectList = []string{"a张三", "1赵六"}
+	expectList = []string{"a张三", "21赵六"}
 	Check(t, list, expectList)
 
 	// 测试边界
@@ -71,7 +71,7 @@ func TestFilterList(t *testing.T) {
 		&SortFilter[*User]{SortType: "desc", SortField: "name"},
 		&PageFilter[*User]{PageNo: 0, PageSize: 10},
 	)
-	expectList = []string{"周八", "郑十", "吴九", "李四", "c孙七", "b王五", "a张三", "1赵六"}
+	expectList = []string{"周八", "郑十", "吴九", "李四", "c孙七", "b王五", "a张三", "21赵六"}
 	Check(t, list, expectList)
 }
 
